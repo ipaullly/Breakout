68015530f4d64c25d832599a34ba4d2b93ddddaa
ipaullly <ipaully22@gmail.com>
2020-02-04 23:19:08 +0300

    editing class variables and erronous method invoking

--- Scene1.js
+++ Scene1.js
@@ -11,18 +11,17 @@ class Scene1 extends Phaser.Scene{
   }
 
   create() {
-    let ball = this.physics.add.sprite(config.width*0.5, config.height-25, 'ball');
-    ball.body.setVelocity(150, -150);
-    ball.setCollideWorldBounds(true);
-    ball.setBounce(1);
+    this.ball = this.physics.add.sprite(config.width*0.5, config.height-25, 'ball');
+    this.ball.body.setVelocity(150, -150);
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1);
 
-    let paddle = this.physics.add.sprite(this.world.width*0.5, this.world.height-5, 'paddle');
-    paddle.setAnchor(0.5, 1);
-    paddle.body.immovable = true;
+    this.paddle = this.physics.add.sprite(config.width*0.5, config.height-5, 'paddle');
+    this.paddle.body.immovable = true;
   }
 
   update() {
-    this.physics.add.collider(ball, paddle);
-    paddle.x = this.input.x || this.world.width*0.5;
+    this.physics.add.collider(this.ball, this.paddle);
+    this.paddle.x = this.input.x || config.width*0.5;
   }
 }
